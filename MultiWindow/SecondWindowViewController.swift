164bc42e5a7d15e7202cf4a0ea5a077f31d09ae3
NeilsUltimateLab <neilsultimatelab@icloud.com>
2019-12-29 20:46:03 +0530

    Initial commit

---
+++ MultiWindow/SecondWindowViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SecondWindowViewController.swift
+//  MultiWindow
+//
+//  Created by Neil Jain on 11/17/18.
+//  Copyright © 2018 Neil Jain. All rights reserved.
+//
+
+import UIKit
+
+class SecondWindowViewController: UIViewController {
+    
+    static func navController() -> UINavigationController {
+        let vc = SecondWindowViewController()
+        vc.view.backgroundColor = .yellow
+        vc.title = "Second Window's Navigation Controller"
+        return UINavigationController(rootViewController: vc)
+    }
+    
+    var button: UIButton = {
+        let button = UIButton(type: .system)
+        button.addTarget(self, action: #selector(buttonAction), for: UIControl.Event.touchUpInside)
+        button.setTitle("Awesome", for: UIControl.State.normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    @objc func buttonAction() {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .red
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addSubview(button)
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        self.navigationController?.navigationBar.delegate = self
+    }
+    
+}
+
+extension SecondWindowViewController: UINavigationBarDelegate {
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .any
+    }
+}
---
+++ MultiWindow/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  MultiWindow
+//
+//  Created by Neil Jain on 11/17/18.
+//  Copyright © 2018 Neil Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    static func navController() -> UINavigationController {
+        let viewController = ViewController()
+        return UINavigationController(rootViewController: viewController)
+    }
+    
+    var button: UIButton = {
+        let button = UIButton(type: .system)
+        button.addTarget(self, action: #selector(buttonAction), for: UIControl.Event.touchUpInside)
+        button.setTitle("Awesome", for: UIControl.State.normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        self.view.backgroundColor = .white
+        self.title = "Window 1's navigation controller"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.view.backgroundColor = .white
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addSubview(button)
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+
+    @objc func buttonAction() {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .red
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+}
+
